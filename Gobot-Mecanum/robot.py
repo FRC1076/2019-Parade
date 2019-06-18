ba39d9a7e5d2d24373a93a4a1a6472ca7b82b253
EPrograming <ethds24@gmail.com>
2019-06-17 20:40:01 -0400

    fixed physics kind of

--- Gobot-Mecanum/robot.py
+++ Gobot-Mecanum/robot.py
@@ -6,10 +6,10 @@ from wpilib.interfaces import GenericHID
 class Robot(wpilib.TimedRobot):
 
     def robotInit(self):
-        front_left_motor =ctre.WPI_TalonSRX(3) #TODO: Give correct values
-        back_left_motor = ctre.WPI_TalonSRX(4) #TODO: Give correct values
-        front_right_motor = ctre.WPI_TalonSRX(1) #TODO: Give correct values
-        back_right_motor = ctre.WPI_TalonSRX(2) #TODO: Give correct values
+        front_left_motor = wpilib.Talon(3) #TODO: Give correct values
+        back_left_motor = wpilib.Talon(4) #TODO: Give correct values
+        front_right_motor = wpilib.Talon(1) #TODO: Give correct values
+        back_right_motor = wpilib.Talon(2) #TODO: Give correct values
 
         front_left_motor.setInverted(True)
         back_left_motor.setInverted(True)
@@ -21,6 +21,8 @@ class Robot(wpilib.TimedRobot):
         self.lstick = wpilib.Joystick(0)
         self.rstick = wpilib.Joystick(1)
 
+        self.gyro = wpilib.AnalogGyro(1)
+
     def teleopInit(self):
         self.xforward = 0
         self.yforward = 0
@@ -29,9 +31,9 @@ class Robot(wpilib.TimedRobot):
 
         """Called when operation control mode is enabled"""
 
-       
+        
         self.drive.driveCartesian(
-            self.lstick.getX(), self.lstick.getY(), self.rstick.getX(), 0
+            self.lstick.getX(), self.lstick.getY(), self.rstick.getX(), self.gyro.getAngle()
         )
         
 if __name__ == "__main__":
