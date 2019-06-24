f56fb45aa52b1d827cd86ae012a75744b0c44ea4
EPrograming <ethds24@gmail.com>
2019-06-24 18:20:11 -0400

    incomeplete_fixes

--- Gobot-Mecanum/robot.py
+++ Gobot-Mecanum/robot.py
@@ -3,19 +3,25 @@ import wpilib.drive
 import ctre
 import robotmap
 
-class Robot(wpilib.TimedRobot):
+class Robot(wpilib.SampleRobot):
 
     def robotInit(self):
-        front_left_motor = robotmap.motor(robotmap.mecanum['front_left_motor']) #TODO: Give correct values
-        back_left_motor = robotmap.motor(robotmap.mecanum['back_left_motor']) #TODO: Give correct values
-        front_right_motor = robotmap.motor(robotmap.mecanum['front_right_motor']) #TODO: Give correct values
-        back_right_motor = robotmap.motor(robotmap.mecanum['back_left_motor']) #TODO: Give correct values
+        front_left_motor = wpilib.Talon(robotmap.mecanum['front_left_motor'])  
+        back_left_motor = wpilib.Talon(robotmap.mecanum['back_left_motor'])  
+        front_right_motor = wpilib.Talon(robotmap.mecanum['front_right_motor'])  
+        back_right_motor = wpilib.Talon(robotmap.mecanum['back_left_motor'])  
 
         front_left_motor.setInverted(True)
         back_left_motor.setInverted(True)
 
 
-        self.drive = wpilib.drive.MecanumDrive(front_left_motor,back_left_motor,front_left_motor,back_right_motor)
+        self.drive = wpilib.drive.MecanumDrive(
+            front_left_motor,
+            back_left_motor,
+            front_left_motor,
+            back_right_motor
+        )
+
         self.drive.setExpiration(0.1)
         
         self.lstick = wpilib.Joystick(0)
@@ -33,18 +39,10 @@ class Robot(wpilib.TimedRobot):
 
         
         self.drive.driveCartesian(
-            self.lstick.getX, self.lstick.getY(), self.rstick.getX(), self.gyro.getAngle()
+            self.lstick.getX(), self.lstick.getY(), self.rstick.getX(), self.gyro.getAngle()
         )
 
-    def deadzone(val, deadzone):
-        if abs(val) < deadzone:
-            return 0
-        elif val < (0):
-            x = ((abs(val) - deadzone)/(1-deadzone))
-            return (-x)
-        else:
-            x = ((val - deadzone)/(1-deadzone))
-            return (x)
+    
         
 if __name__ == "__main__":
     wpilib.run(Robot,physics_enabled=True)
